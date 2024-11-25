847333a1feefbdd8e1fa398f71fb9c99b8cb41ef
Lemonochrome <80280962+Lemonochrme@users.noreply.github.com>
2024-11-25 13:44:11 +0000

    Sprint Boot Boilerplate

---
+++ helpapp/src/main/java/insa/application/helpapp/HelpappApplication.java
@@ -0,0 +1,13 @@
+package insa.application.helpapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpappApplication.class, args);
+	}
+
+}
---
+++ helpapp/src/test/java/insa/application/helpapp/HelpappApplicationTests.java
@@ -0,0 +1,13 @@
+package insa.application.helpapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
