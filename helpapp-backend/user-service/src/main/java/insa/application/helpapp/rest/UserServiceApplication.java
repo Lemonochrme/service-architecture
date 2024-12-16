f9c4ca5033f78a5e8256ee43492dcea22d2ff217
Lemonochrme <robin.marn.pro@gmail.com>
2024-12-16 17:58:51 +0100

    Fixed cors issue with front end hehe

--- helpapp-backend/user-service/src/main/java/insa/application/helpapp/rest/UserServiceApplication.java
+++ helpapp-backend/user-service/src/main/java/insa/application/helpapp/rest/UserServiceApplication.java
@@ -3,6 +3,9 @@ package insa.application.helpapp.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
@@ -17,6 +20,18 @@ public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 
     // Create a new user
