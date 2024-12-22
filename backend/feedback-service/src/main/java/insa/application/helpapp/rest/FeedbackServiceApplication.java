8cf27b7b64b993afae89584b5f6b82157e3451f2
Yohan Boujon <yo.boujon@gmail.com>
2024-12-23 00:15:18 +0100

    Added feedback-service

---
+++ backend/feedback-service/src/main/java/insa/application/helpapp/rest/FeedbackServiceApplication.java
@@ -0,0 +1,36 @@
+package insa.application.helpapp.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SpringBootApplication
+@RestController
+public class FeedbackServiceApplication {
+    @Autowired
+    private AdministrationService administrationService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedbackServiceApplication.class, args);
+    }
+
+    // CORS Configuration
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
+}
